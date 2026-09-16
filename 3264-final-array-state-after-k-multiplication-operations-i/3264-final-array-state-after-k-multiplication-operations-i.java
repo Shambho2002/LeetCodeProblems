dce0315b503eb81880c8d1099112ec2863d44be8
Shambhu Sharma <136264257+Shambho2002@users.noreply.github.com>
2026-09-16 15:22:49 +0530

    Time: 1 ms (98.57%), Space: 47 MB (20.91%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for(int i = 0; i<k; i++){
+			// find index of minimum element
+			int minIndex = 0;
+			
+			for(int j = 1; j<nums.length; j++){
+				if(nums[j] < nums[minIndex]){
+					minIndex = j;
+				}
+			}
+			
+			// Multiply the minimum element
+			nums[minIndex] = nums[minIndex] * multiplier;
+		}
+		return nums;
+    }
+}
