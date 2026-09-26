d79e310965bcc073f1c1c81bcc2df780471ee15d
Shambhu Sharma <136264257+Shambho2002@users.noreply.github.com>
2026-09-26 11:33:43 +0530

    Time: 2 ms (92.37%), Space: 46 MB (96.28%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int row = 0;
+		int col = 0;
+		
+		for(String command: commands){
+			if(command.equals("UP")){
+				row--;
+			}
+			else if(command.equals("DOWN")){
+				row++;
+			}
+			else if(command.equals("LEFT")){
+				col--;
+			}
+			else if(command.equals("RIGHT")){
+				col++;
+			}
+		}
+		int position = (row * n) + col;
+		return position;
+    }
+}
